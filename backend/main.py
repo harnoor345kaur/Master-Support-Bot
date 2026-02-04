775cdbfcb70d10196b9688b7707ba40671f45ebd
harnoor345kaur <kaur.harnoor020@gmail.com>
2026-02-04 12:57:16 +0530

    Updated CORS

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,8 @@ app = FastAPI(title="Master Support Bot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
